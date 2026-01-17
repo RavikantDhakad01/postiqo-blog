55f45311cfcbec01972cde440b6507fcca594c93
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-17 18:56:47 +0530

    Add signup page

---
+++ src/Components/Signup.jsx
@@ -0,0 +1,78 @@
+import { Logo, Input, Button } from './index'
+import { useNavigate, Link } from 'react-router-dom'
+import { useDispatch } from 'react-redux'
+import authService from '../appwrite/auth'
+import { login } from '../store/authslice'
+import { useState } from 'react'
+import { useForm } from 'react-hook-form'
+
+function Signup() {
+
+    const navigate = useNavigate()
+    const [error, setError] = useState("")
+    const dispatch = useDispatch()
+    const { register, handleSubmit } = useForm()
+
+    const create = async (data) => {
+        setError("")
+        try {
+            const session = await authService.createAccount(data)
+            if (session) {
+                const userData = await authService.getCurrentUser()
+                if (userData) {
+                    dispatch(login(userData))
+                }
+                navigate('/')
+            }
+        } catch (error) {
+            setError(error)
+        }
+
+    }
+    return (
+        <div className="flex items-center justify-center">
+            <div className='mx-auto w-full max-w-lg bg-gray-100 rounded-xl p-10 border border-black/10'>
+                <div className="mb-2 flex justify-center">
+                    <span className="inline-block w-full max-w-25">
+                        <Logo width='100%' />
+                    </span>
+                </div>
+                <h2 className="text-center text-2xl font-bold leading-tight">Sign up to create account</h2>
+                <p className="mt-2 text-center text-base text-black/60">
+                    Already have an account?&nbsp;
+                    <Link to="/login" className="font-medium text-primary transition-all duration-200 hover:underline" >
+                        Login
+                    </Link>
+                </p>
+                {error && <p className="text-red-600 mt-8 text-center">{error}</p>}
+                <form onSubmit={handleSubmit(create)}>
+                    <div className='space-y-5'>
+                        <Input type="text" placeholder="Enter your full name" label="Full name:"
+                            {
+                            ...register("name", {
+                                required: true
+                            })
+                            } />
+                        <Input type="email" placeholder="Enter your email" label="Email:"
+                            {
+                            ...register("email", {
+                                required: true,
+                                validate: {
+                                    pattern: (value) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(value) || "Invaild Email formate"
+                                }
+                            })
+                            } />
+                        <Input type="password" placeholder="Enter password" label="Password:"
+                            {
+                            ...register("password", {
+                                required: true
+                            })
+                            } />
+                        <Button children="Sign up" type="submit" className="w-full" />
+                    </div>
+                </form>
+            </div>
+        </div>
+    )
+}
+export default Signup
