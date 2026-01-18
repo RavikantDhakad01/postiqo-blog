d7a9ffd978ce52f5d19da217a7e073d67e8547a1
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-18 09:38:12 +0530

    update index

--- src/Components/index.js
+++ src/Components/index.js
@@ -5,7 +5,8 @@ import Footer from './Footer/Footer'
 import Button from "./Button";
 import Input from "./Input";
 import Logo from './Logo'
-
+import Select from './Select'
+import Rte from './RTE'
 export {
     Container, Header, LogoutBtn, Footer, Button, Input, Logo
 }
