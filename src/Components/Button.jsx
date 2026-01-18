2131bffa5acb8a48ad5554f7e0c9398cfdef4d34
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-18 11:12:51 +0530

    fix syntax error

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,4 +1,4 @@
-function Button({ children, type, bgColor = "bg-blue-600", textColor = "text-white", className = "", ...props }) {
+function Button({ type, bgColor = "bg-blue-600", textColor = "text-white", className = "", ...props }) {
     return (
 
         <button className={`px-4 py-2 rounded-lg ${bgColor} ${textColor} ${className}`} {...props} type={type}>
