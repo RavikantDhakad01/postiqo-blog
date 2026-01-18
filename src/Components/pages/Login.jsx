5388895b71feee3e0ca196e7b9abd16b41ef0004
Ravikant Dhakad <ravikantdhakar456@gmail.com>
2026-01-18 16:53:29 +0530

    Add Login page

---
+++ src/Components/pages/Login.jsx
@@ -0,0 +1,9 @@
+import { Login as loginComponent } from '../index'
+
+export default function Login() {
+    return (
+        <div className='py-8'>
+            <loginComponent />
+        </div>
+    )
+}
